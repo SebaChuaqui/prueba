294ffaa2c84793800b21165a1b04139afff7cb9b
schuaquicano <schuaquicano@gmail.com>
2020-06-18 15:05:30 -0400

    Programa Java Main

---
+++ Ejercicio3.modulo10/src/Main.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] numeros = new int[10];
+        int pos = 0, neg = 0, cero = 0; //contadores
+        int i;
+        
+
+        
+        for (i = 0; i < numeros.length; i++) {
+            System.out.print("numeros[" + i + "]= ");
+            numeros[i] = sc.nextInt();
+        }
+
+        for (i = 0; i < numeros.length; i++) {
+            if (numeros[i] > 0) {
+                pos++;
+            } else if (numeros[i] < 0) {
+                neg++;
+            } else {
+                cero++;
+            }
+        }
+        //mostrar resultados
+        System.out.println("Positivos: " + pos);
+        System.out.println("Negativos: " + neg);
+        System.out.println("Ceros: " + cero);
+    }
+}
