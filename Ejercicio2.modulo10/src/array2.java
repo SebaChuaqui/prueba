daa1b236a1e414dd2ad4436fae66e189e1017687
schuaquicano <schuaquicano@gmail.com>
2020-06-18 15:00:28 -0400

    Programa Java Array 2

---
+++ Ejercicio2.modulo10/src/array2.java
@@ -0,0 +1,26 @@
+
+public class array2 {
+
+	public static void main(String[] args) {
+		
+		int[] numeros=new int[100];
+		
+		int suma=0;
+        double media;
+        
+        for(int i=0;i<numeros.length;i++){
+            numeros[i]=i+1;
+            suma+=numeros[i];
+        }
+        
+        System.out.println("La suma es "+suma);
+        
+        media=((double)suma/numeros.length);
+         
+        System.out.println("La media es "+media);
+         
+        
+
+	}
+
+}
